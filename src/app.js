6a744325f5e89a68142fa220f646a4deb86c5433
Seghrouchni yassir <yassirsegh@gmail.com>
2026-03-31 22:17:24 +0100

    forcing conflict by modifying main branch

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const tasksRouter = require('./routes/tasks');
 
 
 app.get('/', (req, res) => {
-  res.json({ message: "Welcome from MAIN branch" });
+  res.json({ message: "MAIN Version" });
 });
 
 app.use('/tasks', tasksRouter);
